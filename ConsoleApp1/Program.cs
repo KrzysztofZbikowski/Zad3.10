8c4264deb5502891dc60e8a6ec8c601413ce65b3
Zbiku <Zbiku@MSI>
2023-10-26 22:48:40 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        int n, wynik = 1, zm;
+        Console.WriteLine("Podaj liczbę całkowitą");
+        n = int.Parse(Console.ReadLine());
+        zm = n;
+        for (int i = 0; i<n; i++)
+        {
+            wynik = wynik * zm;
+            zm--;
+        }
+        Console.WriteLine("{0} silnia wynosi: {1}", n, wynik);
+        Console.ReadKey();
+    }
+}
